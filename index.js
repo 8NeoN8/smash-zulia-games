313c4307d2d1a111a5aa38755902d170cd0ebe2f
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2025-10-07 18:50:53 -0400

    implement db seach, update db, update list item creation

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import gamesInfo from './games.js'
-
 let helpDialog = document.getElementById('help-dialog')
 let openDialog = document.getElementById('help-button')
 let closeDialog = document.getElementById('close-dialog')
+
+let searchButton = document.getElementById('search-button')
+
+let playerList = []
+let setList = []
+let charList = ['mario','dk','link','samus','dsamus','yoshi','kirby','fox','pikachu','luigi','ness','cf','jigglypuff','peach','daisy','bowser','ics','sheik','zelda','drmario','pichu','falco','marth','lucina','yink','ganondorf','mewtwo','roy','chrom','gnw','mk','pit','dpit','zss','wario','snake','ike','pt','ddk','lucas','sonic','ddd','olimar','lucario','rob','tink','wolf','villager','megaman','wiifit','rosalina','lmac','greninja','mfighter','mgunner','msword','palutena','pac-man','robin','shulk','jr','duckhunt','ryu','ken','cloud','corrin','bayonetta','inkling','ridley','simon','ritcher','kkrool','isabelle','incineroar','plant','joker','hero','bnk','terry','byleth','minmin','steve','sephiroth','pythra','kazuya','sora']
+let tournamentList = []
+
+let char1Filter = document.getElementById('char-filter-one')
+let player1Filter = document.getElementById('player-filter-one')
+let char2Filter = document.getElementById('char-filter-two')
+let player2Filter = document.getElementById('player-filter-two')
+let dateFilter = document.getElementById('date-filter')
+let tournamentFilter = document.getElementById('tournament-filter')
 
 openDialog.addEventListener('click', () => {
   helpDialog.classList.remove('hidden')
@@ -12,7 +24,58 @@ closeDialog.addEventListener('click', () => {
   helpDialog.classList.add('hidden')
 })
 
-testDB()
+searchButton.addEventListener('click', () => {
+  db_searchSets()
+})
+
+db_getPlayerList()
+db_getTournaments()
+charSuggestions()
+
+function getOptionValue(option){
+  return option.value ? option.value : null
+}
+
+function charSuggestions(){
+  for (let i = 0; i < charList.length; i++) {
+    let option = document.createElement('option')
+    option.value = charList[i]
+    option.innerText = charList[i].toUpperCase()
+    char1Filter.appendChild(option)
+  }
+  for (let i = 0; i < charList.length; i++) {
+    let option = document.createElement('option')
+    option.value = charList[i]
+    option.innerText = charList[i].toUpperCase()
+    char2Filter.appendChild(option)
+  }
+}
+
+function playerSuggestions(){
+  for (let i = 0; i < playerList.length; i++) {
+    let option = document.createElement('option')
+    option.value = playerList[i][0]
+    option.innerText = playerList[i][1]
+    option.innerText = option.innerText.toUpperCase()
+    player1Filter.appendChild(option)
+  }
+  for (let i = 0; i < playerList.length; i++) {
+    let option = document.createElement('option')
+    option.value = playerList[i][0]
+    option.innerText = playerList[i][1]
+    option.innerText = option.innerText.toUpperCase()
+    player2Filter.appendChild(option)
+  }
+}
+
+function tourneySuggestions(){
+  for (let i = 0; i < tournamentList.length; i++) {
+    let option = document.createElement('option')
+    option.value = tournamentList[i].id
+    option.innerText = tournamentList[i].name.toUpperCase()
+    tournamentFilter.appendChild(option)
+  }
+}
 
 async function testDB(){
   const db = await getDB()
@@ -26,162 +89,150 @@ async function testDB(){
   console.log(res2);
 }
 
-
 async function getDB(){
   const SQL = await initSqlJs({
     locateFile: file => `https://sql.js.org/dist/${file}`
   });
   
-  const db = new SQL.Database(new Uint8Array( await getDBFile()))
+  const db = new SQL.Database(new Uint8Array(await getDBFile()))
 
   return db
 }
 
 async function getDBFile(){
-  let response = await fetch("https://8neon8.github.io/smash-zulia-games/testdb.db")
+  let response = await fetch("/testdb.db") //*https://8neon8.github.io/smash-zulia-games
   let arrayBuffer = await response.arrayBuffer();
   return arrayBuffer
 }
 
+async function db_getPlayerList(){
+  const db = await getDB()
 
-let setList = gamesInfo.setList
-let showList = []
+  let str = "SELECT * FROM players;"
+  let res = db.exec(str)
 
-//testlink.setAttribute('href',setList[0].link)
+  let values = null
+  let columns = null
+  let objectifiedData = null
 
-let listContainer = document.getElementById('game-list')
+  if(res.length > 0){
+    res = res[0]
+    columns = res.columns
+    values = res.values
 
-let charFilter1 = document.getElementById('char-filter-one')
-let charFilter2 = document.getElementById('char-filter-two')
-
-let playerFilter1 = document.getElementById('player-filter-one')
-let playerFilter2 = document.getElementById('player-filter-two')
-
-let dateFilter = document.getElementById('date-filter')
-let tournamentFilter = document.getElementById('tournament-filter')
-
-let searchButton = document.getElementById('search-button')
-
-let players = []
-let characters = []
-let tournaments = []
-let dates = []
-
-function getSetData(){
-  players = []
-  characters = []
-  tournaments = []
-  dates = []
-
-  for (let i = 0; i < setList.length; i++) {
-    
-    players.push(setList[i].players[0].name)
-    players.push(setList[i].players[1].name)
-  
-    tournaments.push(setList[i].tournament)
-    
-    dates.push(setList[i].date)
-  
-    for (let j = 0; j < setList[i].players.length; j++) {
-      for (let k = 0; k < setList[i].players[j].char.length; k++) {
-        characters.push(setList[i].players[j].char[k]) 
-      }
-    }
-  }
-
-  players = [...new Set(players)]
-  characters = [...new Set(characters)]
-  tournaments = [...new Set(tournaments)]
-  dates = [...new Set(dates)]
-
-}
-
-getSetData()
-
-function createOption(data){
-  let option = document.createElement('option')
-  option.setAttribute('value', data)
-  option.innerText = data.toUpperCase()
-
-  return option
-}
-
-function setDataFields(){
-  for (let i = 0; i < characters.length; i++) {
-    let charOption1 = createOption(characters[i])
-    let charOption2 = createOption(characters[i])
-    charFilter1.appendChild(charOption1)
-    charFilter2.appendChild(charOption2)
-  }
-
-  for (let i = 0; i < players.length; i++) {
-    let playerOption1 = createOption(players[i])
-    let playerOption2 = createOption(players[i])
-    playerFilter1.appendChild(playerOption1)
-    playerFilter2.appendChild(playerOption2)
-  }
-
-  for (let i = 0; i < dates.length; i++) {
-    let dateOption = createOption(dates[i])
-    dateFilter.appendChild(dateOption)
-    
-  }
-
-  for (let i = 0; i < tournaments.length; i++) {
-    let tournamentOption = createOption(tournaments[i])
-    tournamentFilter.appendChild(tournamentOption)
-    
-  }
-}
-
-function getOptionValue(option){
-  return option.value ? option.value : null
-}
-
-function makeSearch(){
-
-  let searchFilter = {}
-
-  let isPlayerOne = getOptionValue(playerFilter1)
-  let isPlayerTwo = getOptionValue(playerFilter2)
-
-  let isCharacterOne = getOptionValue(charFilter1)
-  let isCharacterTwo = getOptionValue(charFilter2)
-
-  let isDate = getOptionValue(dateFilter)
-
-  let isTournament = getOptionValue(tournamentFilter)
-  
-  if(isDate) searchFilter.date = isDate
-  if(isTournament) searchFilter.tournament = isTournament
-
-  if(!isCharacterOne && !isCharacterTwo && !isDate && !isPlayerOne && !isPlayerTwo && !isTournament){
-    showList = setList
-    createListItem(showList)
+    objectifiedData = values.map(row => {
+      const obj = {}
+      columns.forEach((colName, index) => {
+        obj[colName] = row[index]
+      });
+      return obj
+    })
+    playerList = objectifiedData
+    playerSuggestions()
     return
   }
- 
-  let testingNewFilterIdea = setList
+}
+
+async function db_getTournaments(){
+  const db = await getDB()
+  let str = 'SELECT * FROM tournaments'
+  let res = db.exec(str)
   
-  if(isDate) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.date == isDate)
+  let values = null
+  let columns = null 
+  let objectifiedData = null
   
-  if(isTournament) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.tournament == isTournament)
+  if(res.length > 0){
+    res = res[0]
+    columns = res.columns
+    values = res.values
+    
+    objectifiedData = values.map(row => {
+      const obj = {}
+      columns.forEach((colName, index) => {
+        obj[colName] = row[index]
+      });
+      return obj
+    })
+
+    tournamentList = objectifiedData
+    tourneySuggestions()
+    return
+  }
+}
+
+async function db_searchSets(){
+  let isPlayerOne = getOptionValue(player1Filter)
+  let isPlayerTwo = getOptionValue(player2Filter)
+  let isCharacterOne = getOptionValue(char1Filter)
+  let isCharacterTwo = getOptionValue(char2Filter)
+  let isDate = getOptionValue(dateFilter)
+  let isTournament = getOptionValue(tournamentFilter)
   
-  if(isPlayerOne && !isCharacterOne) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.playerOne.name == isPlayerOne || x.playerTwo.name == isPlayerOne)
+  const db = await getDB()
+  let str = 'SELECT * FROM sets WHERE '
+
+  if(isDate){
+
+    let isTourney = tournamentList.findAll(tourney => tourney.date = isDate)
+    console.log(isTourney);
+    if(isTourney.length == 0) return
+    if(isTourney.length == 1) str += `tournament = ${isTourney[0].id} AND `
+    //*make it so if there is more than 1 tournament on the same day it searches for all tournaments
+    //else str += `tournament IN (${isTourney[0].id} AND `
+  }
+
+  if(isPlayerOne && !isPlayerTwo) str += `playerOne = ${isPlayerOne} OR playerTwo = ${isPlayerOne} AND `
+
+  if(isPlayerTwo && !isPlayerOne) str += `playerTwo = ${isPlayerTwo} OR playerOne = ${isPlayerTwo} AND `
+
+  if(isPlayerOne && isPlayerTwo) str += `playerOne = ${isPlayerOne} OR playerTwo = ${isPlayerOne} AND playerTwo = ${isPlayerTwo} OR playerOne = ${isPlayerTwo}`
+
+  if(isCharacterOne && !isCharacterTwo) str += `charOne = '${isCharacterOne}' OR charTwo = '${isCharacterOne}' AND `
+
+  if(isCharacterTwo && !isCharacterOne) str += `charTwo = '${isCharacterTwo}' OR charOne = '${isCharacterTwo}' AND `
+
+  if(isCharacterOne && isCharacterTwo) str += `charOne = '${isCharacterOne}' OR charTwo = '${isCharacterOne}' AND charTwo = '${isCharacterTwo}' OR charOne = '${isCharacterTwo}' AND `
+
+  if(isTournament && !isDate) str += `tournament = '${isTournament}' AND `
+
+  if(str[str.length-1] == ' ' && str[str.length-2] != 'E' && str[str.length-2] != 'D') str = str.slice(0, -2)
+
+  if(str[str.length-1] == ' ' && str[str.length-2] === 'E') str = str.replaceAll(' WHERE ', '')
   
-  if(isPlayerTwo && !isCharacterTwo) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.playerTwo.name == isPlayerTwo || x.playerOne.name == isPlayerTwo)
-      
-  if(isCharacterOne && !isPlayerOne) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.playerOne.char == isCharacterOne || x.playerTwo.char == isCharacterOne)
+  if(str[str.length-1] == ' ' && str[str.length-2] === 'D') str = str.slice(0,-4)
 
-  if(isCharacterTwo && !isPlayerTwo) testingNewFilterIdea = testingNewFilterIdea.filter((x) => x.playerTwo.char == isCharacterTwo || x.playerOne.char == isCharacterTwo)
+  let res = db.exec(str)
 
-  if(isCharacterOne && isPlayerOne) testingNewFilterIdea = testingNewFilterIdea.filter((x) => (x.playerOne.name == isPlayerOne && x.playerOne.char == isCharacterOne) || (x.playerTwo.name == isPlayerOne && x.playerTwo.char == isCharacterOne))
+  let values = null
+  let columns = null
+  let objectifiedData = null
 
-  if(isCharacterTwo && isPlayerTwo) testingNewFilterIdea = testingNewFilterIdea.filter((x) => (x.playerTwo.name == isPlayerTwo && x.playerTwo.char == isCharacterTwo) || (x.playerOne.name == isPlayerTwo && x.playerOne.char == isCharacterTwo))
+  if(res.length > 0){
+    res = res[0]
+    columns = res.columns
+    values = res.values
 
-  showList = testingNewFilterIdea
+    objectifiedData = values.map(row => {
+      const obj = {}
+      columns.forEach((colName, index) => {
+        obj[colName] = row[index]
+      });
+      return obj
+    })
+    setList = objectifiedData
+    //createListItem(setList)
 
-  createListItem(showList)
+    setList.forEach(set => {
+      set.playerOne = playerList.find(player => player.id == set.playerOne)
+      set.playerTwo = playerList.find(player => player.id == set.playerTwo)
+
+      set.tournament = tournamentList.find(tournament => tournament.id == set.tournament)
+    });
+
+    createListItem(setList)
+    return
+  }
 }
 
 function createListItem(itemList){
@@ -205,11 +256,10 @@ function createListItem(itemList){
     playerOneSection.appendChild(playerOneName)
 
     let playerOneCharImg = makeEl('img')
-    //*convert al images to webp, name them exactly like character name to search the image 1:1
-    playerOneCharImg.setAttribute('src',`/img/${itemList[i].playerOne.char}.webp`)
+    playerOneCharImg.setAttribute('src',`/img/${itemList[i].charOne}.webp`)
     addClasses(playerOneCharImg, ['player-character'])
 
-    if(itemList[i].setCount[0] > itemList[i].setCount[1]){
+    if(itemList[i].ScoreOne > itemList[i].scoreTwo){
       addClasses(playerOneCharImg, ['winner-char'])
     }else addClasses(playerOneCharImg, ['loser-char'])
 
@@ -221,26 +271,26 @@ function createListItem(itemList){
 
     let setTournament = makeEl('div')
     addClasses(setTournament, ['set-tournament'])
-    setTournament.setAttribute('title', itemList[i].tournament)
-    setTournament.innerText = itemList[i].tournament
+    setTournament.setAttribute('title', itemList[i].tournament.name)
+    setTournament.innerText = itemList[i].tournament.name
     setData.appendChild(setTournament)
 
     let setDate = makeEl('div')
     addClasses(setDate, ['set-date'])
-    setDate.innerText = itemList[i].date
+    setDate.innerText = itemList[i].tournament.date
     setData.appendChild(setDate)
 
     let setResult = makeEl('div')
     addClasses(setResult, ['set-result'])
     let result1 = makeEl('span')
-    result1.innerText = itemList[i].setCount[0]
+    result1.innerText = itemList[i].scoreOne
     let result2 = makeEl('span')
-    result2.innerText = itemList[i].setCount[1]
+    result2.innerText = itemList[i].scoreTwo
 
     setResult.appendChild(result1)
     setResult.appendChild(result2)
 
-    if(itemList[i].setCount[0] > itemList[i].setCount[1]){
+    if(itemList[i].scoreOne > itemList[i].scoreTwo){
       result1.classList.remove('loser-count')
       addClasses(result1, ['winner-count'])
       result2.classList.remove('winner-count')
@@ -272,11 +322,10 @@ function createListItem(itemList){
     playerTwoSection.appendChild(playerTwoName)
 
     let playerTwoCharImg = makeEl('img')
-    //*convert al images to webp, name them exactly like character name to search the image 1:1
-    playerTwoCharImg.setAttribute('src',`/img/${itemList[i].playerTwo.char}.webp`)
+    playerTwoCharImg.setAttribute('src',`/img/${itemList[i].charTwo}.webp`)
     addClasses(playerTwoCharImg, ['player-character'])
 
-    if(itemList[i].setCount[1] > itemList[i].setCount[0]){
+    if(itemList[i].scoreTwo > itemList[i].scoreOne){
       addClasses(playerTwoCharImg, ['winner-char'])
     }else addClasses(playerTwoCharImg, ['loser-char'])
 
@@ -294,11 +343,4 @@ function addClasses(el, classes){
   for (let i = 0; i < classes.length; i++) {
     el.classList.add(classes[i])
   }
-  
 }
-
-searchButton.addEventListener('click', () => {
-  makeSearch()
-})
-
-setDataFields()
